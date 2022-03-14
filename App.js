3eef344660ef523b1bbdcae21ffb3eef71c72df9
yeranarraila <41158680+yeranarraila@users.noreply.github.com>
2022-03-13 22:31:32 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Tarefa from "./src/pages/Tarefa";
+import NovaTarefa from "./src/pages/NovaTarefa"
+import Detalhes from "./src/pages/Detalhes"
 
+ 
+const Stack = createStackNavigator();
+ 
+//initualRouteName define o nome da primeira pagina do app
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+     <Stack.Navigator initialRouteName="Tarefa">
+      <Stack.Screen
+        name="Tarefas" 
+        component={Tarefa}
+        options={{
+          headerTintColor:"#F92E6A"
+        }}
+      />
+      <Stack.Screen
+        name="Nova Tarefa" 
+        component={NovaTarefa}
+        options={{
+          headerTintColor:"#F92E6A"
+        }}
+      />
+      <Stack.Screen
+        name="Detalhes" 
+        component={Detalhes}
+        options={{
+          headerTintColor:"#F92E6A"
+        }}
+      />
+
+     </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
